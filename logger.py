e21f47d02d9620029f603378b887899c01ac4287
midhudhasa <midhubala@LAPTOP-P211J040.localdomain>
2025-09-06 18:09:40 +0530

    changes added in logger and main.py

--- logger.py
+++ logger.py
@@ -1,1 +1,2 @@
 # This is for logging setting . setting up logging level
+# logger data has been changed in feature1 branch
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 # This is the Entry point to the application
+# feature 1 branch content has been changed . in main
